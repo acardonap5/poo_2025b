43e1f05e2cc633f466dec0328d86822b11da8f11
acardonap5 <acardonap5@miug.edu.gt>
2025-02-16 10:39:58 -0600

    se agregaron comentarios

--- main.cpp
+++ main.cpp
@@ -17,10 +17,13 @@ main(){
 	cout<<"Ingrese Telefono:";
 	cin>>telefono;
 	
+	
 	Cliente obj = Cliente (nombres,apellidos,direccion,telefono,fecha_nacimiento,nit);
 	obj.leer();
 	cout<<"Ingrese el nit a modificar:";
 	cin>>nit;
+	// los metos set se utilizan para modificar los atributos
 	obj.setNit(nit);
+	// los metods get para mostrar el contenido de los atributos
 	cout<<obj.getNit()<<","<<obj.getNombres()<<" "<<obj.getApellidos()<<endl;
 }
